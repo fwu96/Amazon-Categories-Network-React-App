97e904c4d94d1ec51706429383a037ec70f6defa
Feifan Wu <feifanwu96@gmail.com>
2019-12-09 20:10:42 -0600

    Make the filter basic

--- src/Network.js
+++ src/Network.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import * as d3 from "d3";
+import $ from "jquery";
 import "./InfoBox.css";
 import "./Network.css";
 import drawNet from "./Helper";
@@ -8,6 +9,9 @@ class Network extends Component {
     render() {
         const width = window.innerWidth;
         const height = window.innerHeight;
+        $("#numChildren").on("change", function() {
+            console.log(this.value);
+        });
         d3.csv("./all-nodes.csv").then(data => {
             console.log(data);
             let firstLayer = data.filter(obj => +obj["parent"] === 0);
--- src/Vis.js
+++ src/Vis.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Network from './Network';
 import "./Headline.css";
-import "./Filters.css";
+import "./Filters.css"
+import "./bootstrap.css";
 
 const Vis = () => (
     <div className="Vis">
@@ -10,8 +11,15 @@ const Vis = () => (
         </div>
         <div className="filters">
             <h4>Make a Selection of Category: </h4>
-            <select>
-                <option value="default">-----------default------------</option>
+            <select className="custom-select">
+                <option value="default">Choose a category</option>
+            </select>
+            <br/>
+            <h5>Make a Selection of Data Type: </h5>
+            <select className="custom-select-sm">
+                <option value="default">Choose a type</option>
+                <option value="subCat">Number of Sub-Categories</option>
+                <option value="numPro">Number of Products</option>
             </select>
         </div>
         <Network/>
